86eb565c900122e135cf56f1cd09603f8d41a7cb
Navid Ibrahim <nibrahim7787@gmail.com>
2024-09-30 09:43:52 +0600

    Created JSONReader

---
+++ src/main/java/JSONReader.java
@@ -0,0 +1,12 @@
+import org.json.*;
+
+public class JSONReader extends Reader {
+    public JSONReader(String filepath) {
+        super(filepath);
+    }
+
+    @Override
+    public void readFile(StatementAnalyzer statement) {
+
+    }
+}
