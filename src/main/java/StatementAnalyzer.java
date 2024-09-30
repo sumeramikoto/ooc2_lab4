2396e0f10a7001198b0e4dbb2a81db8bce64ccb0
Navid Ibrahim <nibrahim7787@gmail.com>
2024-09-30 08:59:47 +0600

    Added methods

--- src/main/java/StatementAnalyzer.java
+++ src/main/java/StatementAnalyzer.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class StatementAnalyzer {
-    protected List<BankTransaction> listOfBankTransactions = new ArrayList<>();
+    protected List<BankTransaction> listOfBankTransactions;
 
     public List<BankTransaction> getListOfBankTransactions() {
         return listOfBankTransactions;
@@ -10,5 +12,58 @@ public class StatementAnalyzer {
 
     public void setListOfBankTransactions(List<BankTransaction> listOfBankTransactions) {
         this.listOfBankTransactions = listOfBankTransactions;
+    }
+
+    public void determineProfit(List<BankTransaction> listOfBankTransactions) {
+        int finalAmount = 0;
+        for (BankTransaction bankTransaction : listOfBankTransactions) {
+            int transactionAmount = bankTransaction.getAmount();
+            finalAmount += transactionAmount;
+        }
+
+        System.out.println("The final amount is " + finalAmount);
+
+        if (finalAmount < 0) {
+            System.out.println("There is loss");
+        } else {
+            System.out.println("There is profit");
+        }
+    }
+
+    public int getNoOfTransactionsForTheMonth(int month) {
+        int monthlyTransactions = 0;
+        for (BankTransaction bankTransaction : listOfBankTransactions) {
+            int monthOfStatement = bankTransaction.getMonth();
+            if (month == monthOfStatement) {
+                monthlyTransactions++;
+            }
+        }
+        return monthlyTransactions;
+    }
+
+    public void sortExpenses(List<BankTransaction> listOfBankTransactions) {
+        Collections.sort(listOfBankTransactions, Comparator.comparing(BankTransaction::getAmount));
+    }
+
+    public void showTopExpenses(List<BankTransaction> listOfBankTransactions) {
+        sortExpenses(listOfBankTransactions);
+        System.out.println("Top 10 expenses");
+        System.out.println("---------------");
+        for (BankTransaction bankTransaction : listOfBankTransactions) {
+            int index = listOfBankTransactions.indexOf(bankTransaction) + 1;
+            int amount = bankTransaction.getAmount();
+            if (amount > 0 || index == 11) {
+                break;
+            }
+            int expense = Math.abs(amount);
+            String category = bankTransaction.getCategory();
+            System.out.println(index + "." + category + ": " + expense);
+        }
+    }
+
+    public String getMostSpentCategory(List<BankTransaction> listOfBankTransactions) {
+        sortExpenses(listOfBankTransactions);
+        String mostSpentCategory = listOfBankTransactions.get(0).getCategory();
+        return mostSpentCategory;
     }
 }
