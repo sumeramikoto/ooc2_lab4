df5372db6bc343b061212abaf450dc3595b2e49c
Navid Ibrahim <nibrahim7787@gmail.com>
2024-09-30 08:42:08 +0600

    Added file to Reader

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,8 +1,10 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 abstract class Reader {
     protected List<BankTransaction> listOfBankTransactions = new ArrayList<>();
+    File file;
 
     public abstract void readFile();
 }
