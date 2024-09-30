bb6c79a56fa1208f11ecadd35a9358eacf3cde46
Navid Ibrahim <nibrahim7787@gmail.com>
2024-09-30 08:40:28 +0600

    Added fields to Reader

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,3 +1,8 @@
-abstract class Reader {
+import java.util.ArrayList;
+import java.util.List;
 
+abstract class Reader {
+    protected List<BankTransaction> listOfBankTransactions = new ArrayList<>();
+
+    public abstract void readFile();
 }
