8da308ec6dc58fe0c4fa3db3a5b6cdde036f4999
Navid Ibrahim <nibrahim7787@gmail.com>
2024-09-30 08:36:35 +0600

    Added getter/setter to StatementAnalyzer class

--- src/main/java/StatementAnalyzer.java
+++ src/main/java/StatementAnalyzer.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StatementAnalyzer {
+    protected List<BankTransaction> listOfBankTransactions = new ArrayList<>();
+
+    public List<BankTransaction> getListOfBankTransactions() {
+        return listOfBankTransactions;
+    }
+
+    public void setListOfBankTransactions(List<BankTransaction> listOfBankTransactions) {
+        this.listOfBankTransactions = listOfBankTransactions;
+    }
 }
