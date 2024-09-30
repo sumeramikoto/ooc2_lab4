b7c2dd661bb1a72a712e9d34463bdfc3f432d9e2
Navid Ibrahim <nibrahim7787@gmail.com>
2024-09-30 09:44:35 +0600

    Created XMLReader

---
+++ src/main/java/XMLReader.java
@@ -0,0 +1,11 @@
+public class XMLReader extends Reader {
+
+    public XMLReader(String filepath) {
+        super(filepath);
+    }
+
+    @Override
+    public void readFile(StatementAnalyzer statement) {
+
+    }
+}
